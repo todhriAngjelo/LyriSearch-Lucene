7cba731cd49124fbf802582d79a5dff6f39ef16d
Todhri Angjelo <a.todhri@intralot.com>
2023-05-15 14:40:07 +0300

    UI changes

--- src/main/java/gui/CentralWindow.java
+++ src/main/java/gui/CentralWindow.java
@@ -28,134 +28,152 @@ public class CentralWindow extends JFrame implements ActionListener {
 	private final JTextField searchField;
 	private final JComboBox<String> searchByComboBox;
 	private final JButton searchButton;
-	private final JButton recommendationsButton;
+	private final JButton recommendedSongButton;
 	private final JButton indexButton;
 	private final JLabel indexStatusLabel;
 
 	public CentralWindow() {
+		// Add GUI components to the window
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new GridBagLayout());
+
 		// Set window properties
 		setTitle("Lyrics Search");
-		setSize(600, 400);
+		setSize(800, 400);
 		setLocationRelativeTo(null); // center the window
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Create GUI components
-		JPanel headerPanel = new JPanel(new GridBagLayout());
 		ImageIcon logoIcon = new ImageIcon("src/main/resources/LyriSearch header image.PNG");
 		Image logoImage = logoIcon.getImage();
-		int logoWidth = (int) Math.min(0.4 * getWidth(), 0.4 * getHeight()); // calculate logo width based on window size
+		int logoWidth = (int) Math.min(0.6 * getWidth(), 0.6 * getHeight()); // calculate logo width based on window size
 		Image resizedLogoImage = logoImage.getScaledInstance(logoWidth, -1, Image.SCALE_SMOOTH);
 		ImageIcon resizedLogoIcon = new ImageIcon(resizedLogoImage);
 		JLabel logoLabel = new JLabel(resizedLogoIcon);
 
 		GridBagConstraints logoGridBag = new GridBagConstraints();
-		logoGridBag.gridx = 0;
-		logoGridBag.gridy = 0;
-		logoGridBag.insets = new Insets(20, 10, 10, 10);
-		logoGridBag.anchor = GridBagConstraints.PAGE_START;
-		headerPanel.add(logoLabel, logoGridBag);
+		logoGridBag.gridx = 2;
+		logoGridBag.gridy = 1;
+		logoGridBag.insets = new Insets(10, 10, 10, 10);
+		contentPane.add(logoLabel, logoGridBag);
 
 		JLabel titleLabel = new JLabel("Lyrics Search");
 		titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
 		GridBagConstraints titleGridBag = new GridBagConstraints();
-		titleGridBag.gridx = 0;
-		titleGridBag.gridy = -1;
+		titleGridBag.gridx = 2;
+		titleGridBag.gridy = 0;  // Adjust the y-coordinate to change the position
 		titleGridBag.insets = new Insets(10, 10, 10, 10);
-		titleGridBag.anchor = GridBagConstraints.CENTER;
-		headerPanel.add(titleLabel, titleGridBag);
+		contentPane.add(titleLabel, titleGridBag);
 
-		add(headerPanel, BorderLayout.NORTH);
-
-		JLabel searchLabel = new JLabel("Search:");
-		searchField = new JTextField(20);
-		searchByComboBox = new JComboBox<>(
-				new DefaultComboBoxModel<>(new String[]{"Keyword", "Song Title", "Singer"}));
-		searchButton = new JButton("Search");
-		// Set the preferred size for the search button
-		searchButton.setPreferredSize(new Dimension(120, 40));
-
-		recommendationsButton = new JButton("Recommended song for you");
+		recommendedSongButton = new JButton("Recommended song for you");
 		indexButton = new JButton("Index ( only for the first time )");
 		indexStatusLabel = new JLabel(Commons.getIndexInitializedStatus()); // check if index is initiated
 
-		// Add GUI components to the window
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new GridBagLayout());
 
-		GridBagConstraints searchlabelGridBag = new GridBagConstraints();
-		// Search label
-		searchlabelGridBag.gridx = 0;
-		searchlabelGridBag.gridy = 0;
-		searchlabelGridBag.gridwidth = 1;
-		searchlabelGridBag.insets = new Insets(10, 10, 10, 10);
-		searchlabelGridBag.anchor = GridBagConstraints.EAST;
-		contentPane.add(searchLabel, searchlabelGridBag);
-
+		searchField = new JTextField(20);
 		GridBagConstraints searchFieldGridBag = new GridBagConstraints();
-		// Search field
-		searchFieldGridBag.gridx = 1;
-		searchFieldGridBag.gridy = 0;
-		searchFieldGridBag.anchor = GridBagConstraints.WEST;
+		searchFieldGridBag.gridx = 2;
+		searchFieldGridBag.gridy = 2;
 		searchFieldGridBag.fill = GridBagConstraints.HORIZONTAL;
 		searchFieldGridBag.weightx = 1.0;
 		searchFieldGridBag.insets = new Insets(10, 0, 10, 10);
 		contentPane.add(searchField, searchFieldGridBag);
 
+		searchLabel(contentPane);
 
-		GridBagConstraints searchByGridBag = new GridBagConstraints();
-		// Search by combo box
-		searchByGridBag.gridx = 3;
-		searchByGridBag.gridy = 0;
-		searchByGridBag.anchor = GridBagConstraints.EAST;
-		searchByGridBag.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(new JLabel("Search by:"), searchByGridBag);
+		searchBy(contentPane);
 
-		GridBagConstraints searchByComboBoxGridBag = new GridBagConstraints();
-		searchByComboBoxGridBag.gridx = 3;
-		searchByComboBoxGridBag.gridy = 0;
-		searchByComboBoxGridBag.anchor = GridBagConstraints.WEST;
-		searchByComboBoxGridBag.insets = new Insets(10, 0, 10, 10);
-		contentPane.add(searchByComboBox, searchByComboBoxGridBag);
+		searchByComboBox = new JComboBox<>(new DefaultComboBoxModel<>(new String[]{"Keyword", "Song Title", "Singer"}));
+		searchButton = new JButton("Search");
+		searchButton.setPreferredSize(new Dimension(120, 40));
+		searchByComboBox(contentPane);
 
-		GridBagConstraints searchButtonGridBag = new GridBagConstraints();
-		// Search button
-		searchButtonGridBag.gridx = 2;
-		searchButtonGridBag.gridy = 0;
-		searchButtonGridBag.anchor = GridBagConstraints.CENTER;
-		searchButtonGridBag.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(searchButton, searchButtonGridBag);
+		searchButton(contentPane);
 
-		GridBagConstraints indexButtonGridBag = new GridBagConstraints();
-		// Index button
-		indexButtonGridBag.gridx = 0;
-		indexButtonGridBag.gridy = 1;
-		indexButtonGridBag.gridwidth = 2;
-		indexButtonGridBag.fill = GridBagConstraints.HORIZONTAL;
-		indexButtonGridBag.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(indexButton, indexButtonGridBag);
+		indexButton(contentPane);
 
-		GridBagConstraints historyButtonGridBag = new GridBagConstraints();
-		// History button
-		historyButtonGridBag.gridx = 2;
-		historyButtonGridBag.gridy = 1;
-		historyButtonGridBag.gridwidth = 2;
-		historyButtonGridBag.fill = GridBagConstraints.HORIZONTAL;
-		historyButtonGridBag.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(recommendationsButton, historyButtonGridBag);
+		indexStatusLabel(contentPane);
 
-		GridBagConstraints indexStatusLabelGridBag = new GridBagConstraints();
-		// Index status label
-		indexStatusLabelGridBag.gridx = 0;
-		indexStatusLabelGridBag.gridy = 2;
-		indexStatusLabelGridBag.insets = new Insets(10, 10, 10, 10);
-		contentPane.add(indexStatusLabel, indexStatusLabelGridBag);
+		recommendedSongButton(contentPane);
 
 		// Add action listeners to buttons
 		searchButton.addActionListener(this);
-		recommendationsButton.addActionListener(this);
+		recommendedSongButton.addActionListener(this);
 		indexButton.addActionListener(this);
 
-		JButton helpButton = new JButton("?");
+		helpButton(contentPane);
+
+	}
+
+	private void searchByComboBox(Container contentPane) {
+		GridBagConstraints searchByComboBoxGridBag = new GridBagConstraints();
+		searchByComboBoxGridBag.gridx = 5;
+		searchByComboBoxGridBag.gridy = 2;
+		searchByComboBoxGridBag.anchor = GridBagConstraints.WEST;
+		searchByComboBoxGridBag.insets = new Insets(10, 0, 10, 10);
+		contentPane.add(searchByComboBox, searchByComboBoxGridBag);
+	}
+
+	private void searchBy(Container contentPane) {
+		GridBagConstraints searchByGridBag = new GridBagConstraints();
+		// Search by combo box
+		searchByGridBag.gridx = 4;
+		searchByGridBag.gridy = 2;
+		searchByGridBag.anchor = GridBagConstraints.EAST;
+		searchByGridBag.insets = new Insets(10, 10, 10, 10);
+		contentPane.add(new JLabel("Search by:"), searchByGridBag);
+	}
+
+	private void searchLabel(Container contentPane) {
+		JLabel searchLabel = new JLabel("Search:");
+		GridBagConstraints searchlabelGridBag = new GridBagConstraints();
+		// Search label
+		searchlabelGridBag.gridx = 1;
+		searchlabelGridBag.gridy = 2;
+		searchlabelGridBag.gridwidth = 1;
+		searchlabelGridBag.insets = new Insets(10, 10, 10, 10);
+		searchlabelGridBag.anchor = GridBagConstraints.EAST;
+		contentPane.add(searchLabel, searchlabelGridBag);
+	}
+
+	private void searchButton(Container contentPane) {
+		GridBagConstraints searchButtonGridBag = new GridBagConstraints();
+		searchButtonGridBag.gridx = 3;
+		searchButtonGridBag.gridy = 2;
+		searchButtonGridBag.anchor = GridBagConstraints.CENTER;
+		searchButtonGridBag.insets = new Insets(10, 10, 10, 10);
+		contentPane.add(searchButton, searchButtonGridBag);
+	}
+
+	private void indexButton(Container contentPane) {
+		GridBagConstraints indexButtonGridBag = new GridBagConstraints();
+		indexButtonGridBag.gridx = 2;
+		indexButtonGridBag.gridy = 3;
+		indexButtonGridBag.gridwidth = 1;
+		indexButtonGridBag.fill = GridBagConstraints.HORIZONTAL;
+		indexButtonGridBag.insets = new Insets(10, 10, 10, 10);
+		contentPane.add(indexButton, indexButtonGridBag);
+	}
+
+	private void recommendedSongButton(Container contentPane) {
+		GridBagConstraints recommnededSongButtonGridBag = new GridBagConstraints();
+		recommnededSongButtonGridBag.gridx = 3;
+		recommnededSongButtonGridBag.gridy = 3;
+		recommnededSongButtonGridBag.gridwidth = 2;
+		recommnededSongButtonGridBag.fill = GridBagConstraints.HORIZONTAL;
+		recommnededSongButtonGridBag.insets = new Insets(10, 10, 10, 10);
+		contentPane.add(recommendedSongButton, recommnededSongButtonGridBag);
+	}
+
+	private void indexStatusLabel(Container contentPane) {
+		GridBagConstraints indexStatusLabelGridBag = new GridBagConstraints();
+		// Index status label
+		indexStatusLabelGridBag.gridx = 2;
+		indexStatusLabelGridBag.gridy = 4;
+		contentPane.add(indexStatusLabel, indexStatusLabelGridBag);
+	}
+
+	private void helpButton(Container contentPane) {
+		JButton helpButton = new JButton("Help ?");
 		helpButton.addActionListener(e -> JOptionPane.showMessageDialog(CentralWindow.this, "Usage details:\n\n" +
 				"1. Store the CSV file containing the lyrics data in the root directory of the project.\n" +
 				"2. Click the 'Index' button to initialize the search index.\n" +
@@ -165,12 +183,11 @@ public class CentralWindow extends JFrame implements ActionListener {
 				"6. Use the 'History' button to view previous search results.", "Usage Details", JOptionPane.INFORMATION_MESSAGE));
 
 		GridBagConstraints helpButtonGridBag = new GridBagConstraints(); // Help button
-		helpButtonGridBag.gridx = 4;
-		helpButtonGridBag.gridy = 2;
+		helpButtonGridBag.gridx = 5;
+		helpButtonGridBag.gridy = 3;
 		helpButtonGridBag.anchor = GridBagConstraints.EAST;
 		helpButtonGridBag.insets = new Insets(10, 0, 10, 10);
 		contentPane.add(helpButton, helpButtonGridBag);
-
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -211,7 +228,7 @@ public class CentralWindow extends JFrame implements ActionListener {
 			// Display the first page of results
 			displayResults(searchTerm, results, currentPage[0], pageSize, null);
 
-		} else if (e.getSource() == recommendationsButton) {
+		} else if (e.getSource() == recommendedSongButton) {
 			if (Commons.SEARCH_HISTORY.isEmpty()) {
 				JOptionPane.showMessageDialog(this, "No search history available.");
 				return;
@@ -297,15 +314,106 @@ public class CentralWindow extends JFrame implements ActionListener {
 		return mostFrequentSong;
 	}
 
+//	private void displayResults(String searchTerm, List<SearchResult> results, int currentPage, int pageSize, JFrame previousDialog) {
+//		// Create a dialog to hold the result scroll pane and pagination controls
+//		JFrame dialog = new JFrame();
+//		dialog.setTitle("Search Results - Page " + currentPage);
+//		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//		dialog.setLayout(new BorderLayout());
+//		dialog.pack();
+//		dialog.setLocationRelativeTo(null); // Center the dialog on the screen
+//		dialog.setVisible(true);
+//
+//		if (previousDialog != null) {
+//			previousDialog.dispose();
+//		}
+//
+//		int numResults = results.size();
+//		int numPages = (int) Math.ceil((double) numResults / pageSize);
+//		int startIndex = (currentPage - 1) * pageSize;
+//		int endIndex = Math.min(startIndex + pageSize, numResults);
+//		List<SearchResult> pageResults = results.subList(startIndex, endIndex);
+//
+//		StringBuilder resultText = new StringBuilder("<html><body>");
+//		for (int i = 0; i < pageResults.size(); i++) {
+//			SearchResult result = pageResults.get(i);
+//			String songTitle = result.getSongTitle().replaceAll("(?i)" + Pattern.quote(searchTerm), "<b style='font-weight: bold;'>$0</b>");
+//			String singer = result.getSinger().replaceAll("(?i)" + Pattern.quote(searchTerm), "<b style='font-weight: bold;'>$0</b>");
+//			String lyrics = result.getLyrics()
+//					.replaceAll("(?i)" + Pattern.quote(searchTerm), "<b style='font-weight: bold;'>$0</b>")
+//					.replaceAll("(?m)^\\s+", "") // remove all leading whitespace
+//					.replaceAll("(?m)$", "<br/>"); // replace line ends with <br/>
+//
+//			String bgColor = i % 2 == 0 ? "#f2f2f2" : "white";
+//			resultText.append("<div style='background-color:").append(bgColor).append(";'>");
+//			resultText.append("<p><i>").append(singer).append(" - </i> <i>").append(songTitle).append("</i></p><p>").append(lyrics).append("</p><hr/>");
+//			resultText.append("</div>");
+//		}
+//		resultText.append("</body></html>");
+//
+//		JEditorPane resultEditorPane = new JEditorPane();
+//		resultEditorPane.setContentType("text/html");
+//		resultEditorPane.setText(resultText.toString());
+//		resultEditorPane.setEditable(false);
+//		resultEditorPane.setPreferredSize(new Dimension(600, Integer.MAX_VALUE));
+//
+//		JScrollPane resultScrollPane = new JScrollPane(resultEditorPane);
+//		resultScrollPane.setPreferredSize(new Dimension(600, 400));
+//
+//		// Create the pagination controls
+//		JPanel paginationPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+//		JButton prevButton = new JButton("Prev");
+//		JButton nextButton = new JButton("Next");
+//		JLabel pageLabel = new JLabel("Page " + currentPage + " of " + numPages);
+//
+//		paginationPanel.add(prevButton);
+//		paginationPanel.add(pageLabel);
+//		paginationPanel.add(nextButton);
+//
+//		// Add action listeners to the pagination buttons
+//
+//		final int[] finalCurrentPage = {currentPage};
+//		prevButton.addActionListener(e -> {
+//			if (finalCurrentPage[0] > 1) {
+//				finalCurrentPage[0]--;
+//				displayResults(searchTerm, results, finalCurrentPage[0], pageSize, dialog);
+//				pageLabel.setText("Page " + finalCurrentPage[0] + " of " + numPages);
+//			}
+//		});
+//
+//		nextButton.addActionListener(e -> {
+//			if (finalCurrentPage[0] < numPages) {
+//				finalCurrentPage[0]++;
+//				displayResults(searchTerm, results, finalCurrentPage[0], pageSize, dialog);
+//				pageLabel.setText("Page " + finalCurrentPage[0] + " of " + numPages);
+//			}
+//		});
+//
+//		// Create a panel to hold the result scroll pane and pagination controls
+//		JPanel contentPanel = new JPanel(new BorderLayout());
+//		contentPanel.add(resultScrollPane, BorderLayout.CENTER);
+//		contentPanel.add(paginationPanel, BorderLayout.SOUTH);
+//
+//		// Create a panel to hold the result scroll pane
+//		JPanel resultPanel = new JPanel(new BorderLayout());
+//		resultPanel.add(resultScrollPane, BorderLayout.CENTER);
+//
+//		// Add the result panel and pagination panel to the dialog
+//		dialog.add(resultPanel, BorderLayout.CENTER);
+//		dialog.add(paginationPanel, BorderLayout.SOUTH);
+//
+//		// Pack and display the dialog
+//		dialog.pack();
+//		dialog.setLocationRelativeTo(null); // Center the dialog on the screen
+//		dialog.setVisible(true);
+//	}
+
 	private void displayResults(String searchTerm, List<SearchResult> results, int currentPage, int pageSize, JFrame previousDialog) {
 		// Create a dialog to hold the result scroll pane and pagination controls
 		JFrame dialog = new JFrame();
 		dialog.setTitle("Search Results - Page " + currentPage);
 		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		dialog.setLayout(new BorderLayout());
-		dialog.pack();
-		dialog.setLocationRelativeTo(null); // Center the dialog on the screen
-		dialog.setVisible(true);
 
 		if (previousDialog != null) {
 			previousDialog.dispose();
@@ -338,7 +446,6 @@ public class CentralWindow extends JFrame implements ActionListener {
 		resultEditorPane.setContentType("text/html");
 		resultEditorPane.setText(resultText.toString());
 		resultEditorPane.setEditable(false);
-		resultEditorPane.setPreferredSize(new Dimension(600, Integer.MAX_VALUE));
 
 		JScrollPane resultScrollPane = new JScrollPane(resultEditorPane);
 		resultScrollPane.setPreferredSize(new Dimension(600, 400));
@@ -348,12 +455,14 @@ public class CentralWindow extends JFrame implements ActionListener {
 		JButton prevButton = new JButton("Prev");
 		JButton nextButton = new JButton("Next");
 		JLabel pageLabel = new JLabel("Page " + currentPage + " of " + numPages);
+		JLabel totalResultsLabel = new JLabel("Total Results: " + numResults);
+		JLabel rangeLabel = new JLabel("Results Range: " + (startIndex + 1) + " - " + endIndex);
 
 		paginationPanel.add(prevButton);
 		paginationPanel.add(pageLabel);
 		paginationPanel.add(nextButton);
-
-		// Add action listeners to the pagination buttons
+		paginationPanel.add(totalResultsLabel);
+		paginationPanel.add(rangeLabel);
 
 		final int[] finalCurrentPage = {currentPage};
 		prevButton.addActionListener(e -> {
@@ -377,18 +486,13 @@ public class CentralWindow extends JFrame implements ActionListener {
 		contentPanel.add(resultScrollPane, BorderLayout.CENTER);
 		contentPanel.add(paginationPanel, BorderLayout.SOUTH);
 
-		// Create a panel to hold the result scroll pane
-		JPanel resultPanel = new JPanel(new BorderLayout());
-		resultPanel.add(resultScrollPane, BorderLayout.CENTER);
-
-		// Add the result panel and pagination panel to the dialog
-		dialog.add(resultPanel, BorderLayout.CENTER);
-		dialog.add(paginationPanel, BorderLayout.SOUTH);
+		// Add the content panel to the dialog
+		dialog.add(contentPanel, BorderLayout.CENTER);
 
 		// Pack and display the dialog
 		dialog.pack();
+		dialog.setLocationRelativeTo(null); // Center the dialog on the screen
 		dialog.setVisible(true);
-
 	}
 
 
